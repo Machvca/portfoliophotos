113f4de9bc22fb019e925572764afb0a61591350
Jorge Machuca <jjorgemachuca@gmail.com>
2025-10-07 21:51:22 +0200

    norway photos

--- app/norway/page.tsx
+++ app/norway/page.tsx
@@ -5,15 +5,15 @@ import Gallery from "../components/Gallery"; // ajusta según tu ruta real
 const norwayGallery = () => (
   <Gallery
     photos={[
-      { src: "/images/milan1.webp", span: "col-span-1 row-span-1" },
-      { src: "/images/milan5.webp", span: "col-span-2 row-span-2" },
-      { src: "/images/milan3.webp", span: "col-span-1 row-span-2" },
-      { src: "/images/milan4.webp", span: "col-span-1 row-span-3" },
+      { src: "/images/nor1.webp", span: "col-span-1 row-span-1" },
+      { src: "/images/nor5.webp", span: "col-span-2 row-span-2" },
+      { src: "/images/nor3.webp", span: "col-span-1 row-span-2" },
+      { src: "/images/nor2.webp", span: "col-span-1 row-span-3" },
       {
-        src: "/images/milan2.webp",
+        src: "/images/nor6.webp",
         span: "col-span-2 md:col-span-3 row-span-3",
       },
-      { src: "/images/milan6.webp", span: "col-span-1 row-span-2" },
+      { src: "/images/nor4.webp", span: "col-span-1 row-span-2" },
     ]}
     previousHref="/travel"
     nextHref="/milan"
